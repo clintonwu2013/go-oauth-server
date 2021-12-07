5c4fee17472a2e0218a44f07798468c6b50948cf
timowu <timowu@changingtec.com>
2021-12-07 10:00:48 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module goOauthServer
+
+go 1.16
+
+require (
+	github.com/go-oauth2/oauth2/v4 v4.4.2 // indirect
+	github.com/go-session/session v3.1.2+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/tidwall/btree v0.7.1 // indirect
+	github.com/tidwall/buntdb v1.2.7 // indirect
+	github.com/tidwall/gjson v1.12.1 // indirect
+	github.com/tidwall/rtree v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20211201190559-0a0e4e1bb54c // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ server.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/generates"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
+	"github.com/go-session/session"
+)
+
+var (
+	portvar               = 9096
+	client_id             = "222222"
+	client_secret         = "22222222"
+	doamin                = "http://localhost:9094"
+	redirect_uri          = "http://localhost:9094/oauth2"
+	code_challenge        = "Qn3Kywp0OiU4NK_AFzGPlmrcYJDJ13Abj_jdL08Ahg8"
+	scope                 = "all"
+	code_challenge_method = "S256"
+	state                 = "xyz"
+)
+
+func main() {
+
+	manager := manage.NewDefaultManager()
+	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+
+	// token store
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+	manager.MapAccessGenerate(generates.NewAccessGenerate())
+
+	clientStore := store.NewClientStore()
+	clientStore.Set(client_id, &models.Client{
+		ID:     client_id,
+		Secret: client_secret,
+		Domain: doamin,
+	})
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewServer(server.NewConfig(), manager)
+
+	srv.SetUserAuthorizationHandler(userAuthorizeHandler)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/auth", authHandler)
+
+	http.HandleFunc("/oauth/authorize", func(w http.ResponseWriter, r *http.Request) {
+		store, err := session.Start(r.Context(), w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var form url.Values
+		if v, ok := store.Get("ClientData"); ok {
+			form = v.(url.Values)
+		}
+		r.Form = form
+		store.Delete("ClientData")
+		store.Save()
+
+		err = srv.HandleAuthorizeRequest(w, r)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	})
+
+	http.HandleFunc("/oauth/token", func(w http.ResponseWriter, r *http.Request) {
+
+		grantType := r.FormValue("grant_type")
+		if grantType != "authorization_code" {
+			http.Error(w, "accept only grant type: authorization_code", http.StatusInternalServerError)
+			return
+		}
+		ctx := r.Context()
+
+		gt, tgr, err := srv.ValidationTokenRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		ti, err := srv.GetAccessToken(ctx, gt, tgr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tokenData := srv.GetTokenData(ti)
+
+		fmt.Println("tokenData[access_token]=", tokenData["access_token"])
+		fmt.Println("tokenData[expires_in]=", tokenData["expires_in"])
+
+		err = returnAccessTokenData(w, tokenData, nil)
+		//err := srv.HandleTokenRequest(w, r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+
+		token, err := srv.ValidationBearerToken(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		data := map[string]interface{}{
+			"expires_in": int64(token.GetAccessCreateAt().Add(token.GetAccessExpiresIn()).Sub(time.Now()).Seconds()),
+			"client_id":  token.GetClientID(),
+			"user_id":    token.GetUserID(),
+		}
+		e := json.NewEncoder(w)
+		e.SetIndent("", "  ")
+		e.Encode(data)
+	})
+
+	log.Printf("Server is running at %d port.\n", portvar)
+	log.Printf("Point your OAuth client Auth endpoint to %s:%d%s", "http://localhost", portvar, "/oauth/authorize")
+	log.Printf("Point your OAuth client Token endpoint to %s:%d%s", "http://localhost", portvar, "/oauth/token")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portvar), nil))
+}
+
+func returnAccessTokenData(w http.ResponseWriter, data map[string]interface{}, header http.Header, statusCode ...int) error {
+
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.Header().Set("Cache-Control", "no-store")
+	w.Header().Set("Pragma", "no-cache")
+
+	for key := range header {
+		w.Header().Set(key, header.Get(key))
+	}
+
+	status := http.StatusOK
+	if len(statusCode) > 0 && statusCode[0] > 0 {
+		status = statusCode[0]
+	}
+
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		return
+	}
+
+	uid, ok := store.Get("LoggedInUserID")
+	if !ok {
+		if r.Form == nil {
+			r.ParseForm()
+		}
+		store.Set("ClientData", r.Form)
+		store.Save()
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	userID = uid.(string)
+	store.Delete("LoggedInUserID")
+	store.Save()
+	return
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	store, err := session.Start(r.Context(), w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if r.Method == "POST" {
+		//TODO: VERIFY IN ide AND GET UID
+		uid := "1"
+		store.Set("LoggedInUserID", uid)
+		store.Save()
+		w.Header().Set("Location", "/auth")
+		w.WriteHeader(http.StatusFound)
+
+		return
+	}
+	outputHTML(w, r, "static/login.html")
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	store, err := session.Start(nil, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if _, ok := store.Get("LoggedInUserID"); !ok {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	outputHTML(w, r, "static/auth.html")
+}
+
+func outputHTML(w http.ResponseWriter, req *http.Request, filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer file.Close()
+	fi, _ := file.Stat()
+	http.ServeContent(w, req, file.Name(), fi.ModTime(), file)
+}
